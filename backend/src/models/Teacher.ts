7d33f22d37647c6b086b82e5297afdf604d6aef9
waqar nawaz <waqarawkums@gmail.com>
2026-09-25 02:42:34 +0500

    Harden staff and parent branch integrity

--- backend/src/models/Teacher.ts
+++ backend/src/models/Teacher.ts
@@ -1,9 +1,14 @@
 import { Table, Column, Unique, Index, DataType, ForeignKey, BelongsTo } from "sequelize-typescript";
 import { BaseModel } from "./BaseModel";
 import { User } from "./User";
+import { Branch } from "./Branch";
 
 @Table({ tableName: "teachers" })
 export class Teacher extends BaseModel {
+  @ForeignKey(() => Branch)
+  @Column({ type: DataType.BIGINT.UNSIGNED })
+  branchId!: number;
+
   @Unique
   @Column({ type: DataType.STRING(30), allowNull: false })
   staffNo!: string;
