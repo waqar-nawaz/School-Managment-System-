6e37334a965d43c7051fb0bc9c10cd8f282fa20d
waqar nawaz <waqarawkums@gmail.com>
2026-09-25 03:14:24 +0500

    Add branch to book copies

--- backend/src/models/BookCopy.ts
+++ backend/src/models/BookCopy.ts
@@ -1,9 +1,13 @@
 import { Table, Column, Unique, DataType, ForeignKey } from "sequelize-typescript";
 import { BaseModel } from "./BaseModel";
 import { Book } from "./Book";
+import { Branch } from "./Branch";
 
 @Table({ tableName: "book_copies" })
 export class BookCopy extends BaseModel {
+  @ForeignKey(() => Branch)
+  @Column({ type: DataType.BIGINT.UNSIGNED })
+  branchId!: number;
   @Unique
   @Column({ type: DataType.STRING(30), allowNull: false })
   accessionNo!: string;
